88f3662a96459ef36767c980c9001efef5f9852a
Andrew Ferguson <aferguson@umail.ucsb.com>
2015-01-28 15:15:16 -0800

    moved robot image location

--- Enemy.java
+++ Enemy.java
@@ -10,7 +10,7 @@ public class Enemy {
     boolean attacking = false;
     
     public Enemy() {
-	ImageIcon i = new ImageIcon("images/playerImages/guy/robot.png"); //enemy image
+	ImageIcon i = new ImageIcon("images/enemyImages/robot/robotFront.png"); //enemy image
 	still = i.getImage();
         //starting Enemy postion/velocity/acceleration
 	x = 700;
