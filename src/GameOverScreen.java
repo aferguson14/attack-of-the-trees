ad6712a9d506152a10ddefefca6f4c11cb175f5e
Andrew Ferguson <aferguson@umail.ucsb.edu>
2015-03-03 21:23:13 -0800

    test2

---
+++ src/GameOverScreen.java
@@ -0,0 +1,107 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.border.Border;
+
+public class GameOverScreen extends JFrame {
+
+	public JPanel buttonPanel;
+	public JPanel labelPanel;
+	public JButton playAgainButton;
+	public JButton exitButton;
+	public JLabel label;
+
+	public GameOverScreen() {
+		super();
+		// set frame characteristics
+		this.setTitle("Game Over Screen");
+		this.setSize(750, 300);
+
+		// set location of game over screen to middle of board
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+        this.setLocationRelativeTo(null);
+
+//	    this.setVisible(true);
+		this.setFocusable(true);
+		this.setUndecorated(true);
+
+		// create background panel to hold
+		JPanel bkgd = new JPanel();
+		bkgd.setLayout(new BorderLayout());
+		this.getContentPane().add(bkgd, BorderLayout.CENTER);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		// set layout for adding buttons to panel
+		buttonPanel = new JPanel();
+		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+		buttonPanel.setBorder(BorderFactory.createEmptyBorder(0,10,10,10));
+
+		// add buttons to buttonPanel
+		buttonPanel.add(Box.createHorizontalGlue());
+		playAgainButton = new JButton("Play Again");
+		buttonPanel.add(playAgainButton);
+		buttonPanel.add(Box.createRigidArea(new Dimension(10,0)));
+		exitButton = new JButton("Exit Game");
+		buttonPanel.add(exitButton);
+		// add listeners to button
+		playAgainButton.addActionListener(new PAL());
+		exitButton.addActionListener(new EL());		
+		// add buttonPanel to background
+		bkgd.add(buttonPanel, BorderLayout.SOUTH);
+
+		// add label that says "GAME OVER" in center
+		labelPanel = new JPanel();
+		label = new JLabel("GAME OVER");
+		label.setFont(new Font("arial", Font.BOLD, 120));
+		label.setForeground(new Color(0x000066));
+		Border labelBorder = BorderFactory.createLineBorder(new Color(0xBAE6DA), 8);
+		label.setBorder(labelBorder);
+		labelPanel.add(label);
+		// add labelPanel to background
+		bkgd.add(labelPanel, BorderLayout.CENTER);
+
+		this.pack();
+	}
+
+	/*
+	 * @param e ActionEvent
+	 */	
+	public void displayGOScreen(ActionEvent e) {
+		this.setVisible(true);
+	}
+
+	/*
+	 * @param e ActionEvent
+	 */	
+	public void playAgain(ActionEvent e) {
+		this.dispose();
+		Board.setState(Board.STATE.MENU);
+	}
+
+	/*
+	 * @param g Graphics object g
+	 */
+	public void paintComponent(Graphics g) {	
+		super.paint(g);
+	}
+
+	private class PAL implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (Board.getState() == Board.STATE.GAMEOVER)
+				playAgain(e);
+//			else if (Board.getState() == Board.STATE.GAME)
+//				displayGOScreen(e);
+		}	
+	} //PAL
+
+	private class EL implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.exit(0);			
+		}			
+	} //EL
+
+} //GOScreen
---
+++ src/Menu.java
@@ -0,0 +1,63 @@
+import java.awt.*;
+
+public class Menu {
+    //Menu buttons
+    public Rectangle newGameButtonOutline = new Rectangle((Board.WIDTH/2+98), 248, 303, 73);
+    public Rectangle newGameButtonInline = new Rectangle((Board.WIDTH/2+102), 252, 296, 66);
+    public Rectangle newGameButton = new Rectangle((Board.WIDTH/2+100), 250, 300, 70);
+    public Rectangle loadGameButtonOutline = new Rectangle((Board.WIDTH/2+98), 398, 303, 73);
+    public Rectangle loadGameButtonInline = new Rectangle((Board.WIDTH/2+102), 402, 296, 66);
+    public Rectangle loadGameButton = new Rectangle((Board.WIDTH/2+100), 400, 300, 70);
+    private int x = -100;
+    private int y = 150;
+    private int ngx = newGameButton.x+15;
+    private int ngy = newGameButton.y+50;
+    private int lgx = loadGameButton.x+15;
+    private int lgy = loadGameButton.y+50;
+    
+    public void render(Graphics g) {
+        //Paints Menu
+	Graphics2D g2d = (Graphics2D) g; 
+	Stroke oldStroke = g2d.getStroke();
+	
+	Font fnt0 = new Font("arial", Font.BOLD, 80);
+	g.setFont(fnt0);
+	g.setColor(new Color(0x000066));
+	g.drawString("Attack of the Trees", x-2, y-2);
+	g.drawString("Attack of the Trees", x-2, y+2);
+	g.drawString("Attack of the Trees", x+2, y-2);
+	g.drawString("Attack of the Trees", x+2, y+2);
+	g.setColor(new Color(0xEEFFEE));
+	g.drawString("Attack of the Trees", x, y);
+	
+	Font fnt1 = new Font("arial", Font.BOLD, 50);
+	g.setFont(fnt1);
+	
+	g.setColor(new Color(0x000066));
+	g.drawString("New Game", ngx-1, ngy-1);
+	g.drawString("New Game", ngx-1, ngy+1);
+	g.drawString("New Game", ngx+1, ngy-1);
+	g.drawString("New Game", ngx+1, ngy+1);
+	
+	g.setColor(new Color(0x000066));
+	g.drawString("Load Game", lgx-1, lgy-1);
+	g.drawString("Load Game", lgx-1, lgy+1);
+	g.drawString("Load Game", lgx+1, lgy-1);
+	g.drawString("Load Game", lgx+1, lgy+1);
+	
+	g.setColor(new Color(0xEEFFEE));
+	g.drawString("New Game", ngx, ngy);
+	g.drawString("Load Game", lgx, lgy);
+	
+	g2d.setStroke(new BasicStroke(4));
+	g2d.draw(loadGameButton);
+	g2d.draw(newGameButton);
+	g2d.setColor(new Color(0x000066));
+	g2d.setStroke(oldStroke);
+	g2d.draw(newGameButtonOutline);
+	g2d.draw(newGameButtonInline);
+	g2d.draw(loadGameButtonOutline);
+	g2d.draw(loadGameButtonInline);
+	g2d.setStroke(oldStroke);
+    }
+}
