ea5eeb533ac0c99cd2d40c8baffd3e0a2a82b71e
Shadee Barzin <shadeebarzin@umail.ucsb.edu>
2015-03-06 11:02:39 -0800

    changed button size

--- src/Menu.java
+++ src/Menu.java
@@ -2,17 +2,17 @@ import java.awt.*;
 
 public class Menu {
     //Menu buttons
-    public Rectangle newGameButtonOutline = new Rectangle((Board.WIDTH/2+98), 248, 303, 73);
-    public Rectangle newGameButtonInline = new Rectangle((Board.WIDTH/2+102), 252, 296, 66);
-    public Rectangle newGameButton = new Rectangle((Board.WIDTH/2+100), 250, 300, 70);
-    public Rectangle loadGameButtonOutline = new Rectangle((Board.WIDTH/2+98), 398, 303, 73);
-    public Rectangle loadGameButtonInline = new Rectangle((Board.WIDTH/2+102), 402, 296, 66);
-    public Rectangle loadGameButton = new Rectangle((Board.WIDTH/2+100), 400, 300, 70);
+    public Rectangle newGameButtonOutline = new Rectangle((Board.WIDTH/2+98), 248, 403, 73);
+    public Rectangle newGameButtonInline = new Rectangle((Board.WIDTH/2+102), 252, 396, 66);
+    public Rectangle newGameButton = new Rectangle((Board.WIDTH/2+100), 250, 400, 70);
+    public Rectangle loadGameButtonOutline = new Rectangle((Board.WIDTH/2+98), 398, 403, 73);
+    public Rectangle loadGameButtonInline = new Rectangle((Board.WIDTH/2+102), 402, 396, 66);
+    public Rectangle loadGameButton = new Rectangle((Board.WIDTH/2+100), 400, 400, 70);
     private int x = -100;
     private int y = 150;
-    private int ngx = newGameButton.x+15;
+    private int ngx = newGameButton.x+47;
     private int ngy = newGameButton.y+50;
-    private int lgx = loadGameButton.x+15;
+    private int lgx = loadGameButton.x+47;
     private int lgy = loadGameButton.y+50;
     
     public void render(Graphics g) {
