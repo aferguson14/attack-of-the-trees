c624cd3220bfd4c83160b6cb81a5f615241eae1c
Shadee Barzin <shadeebarzin@umail.ucsb.edu>
2015-03-11 22:09:57 -0700

    added javadoc comment for class

--- src/EnemyBee.java
+++ src/EnemyBee.java
@@ -1,12 +1,20 @@
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.io.Serializable;
-
 import static java.lang.Math.abs;
-
 import java.util.ArrayList;
-
 import javax.swing.ImageIcon;
+
+/** 
+ * EnemyBee represents the bee enemy the player must defeat.
+ * 
+ * @author Shadee Barzin
+ * @author Andrew Ferguson
+ * @author Michele Haque
+ * @author Brendan Murphy
+ * @author Fengyu Wang
+ * @version CS 48, Winter 2015
+*/
 
 public class EnemyBee extends Enemies implements Serializable{
 	private boolean allgood = false;
