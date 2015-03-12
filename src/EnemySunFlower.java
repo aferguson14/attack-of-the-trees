f453ec8aa38b859c534886af02049a02393552ce
Shadee Barzin <shadeebarzin@umail.ucsb.edu>
2015-03-11 22:11:24 -0700

    added javadoc comment for class

--- src/EnemySunFlower.java
+++ src/EnemySunFlower.java
@@ -1,12 +1,20 @@
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.io.Serializable;
-
 import static java.lang.Math.abs;
-
 import java.util.ArrayList;
-
 import javax.swing.ImageIcon;
+
+/** 
+ * EnemySunFlower represents the sunflower enemy the player must defeat.
+ * 
+ * @author Shadee Barzin
+ * @author Andrew Ferguson
+ * @author Michele Haque
+ * @author Brendan Murphy
+ * @author Fengyu Wang
+ * @version CS 48, Winter 2015
+*/
 
 public class EnemySunFlower extends Enemies implements Serializable{
 
