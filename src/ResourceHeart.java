6d382ab03d6e240f16492097afa317261fd55650
Michele Haque <michelehaque@engineering.ucsb.edu>
2015-03-13 10:59:40 -0700

    Updated javadoc comments for methods

--- src/ResourceHeart.java
+++ src/ResourceHeart.java
@@ -16,6 +16,11 @@ import javax.swing.ImageIcon;
 
 public class ResourceHeart extends Resource implements Serializable{
 
+    /**
+     * @param x double for x coordinate
+     * @param y double for y coordinate
+     * Creates a heart resource that sets it coordinates, image, and type
+     */
     public ResourceHeart(double x, double y){
 	super(x,y);
 	//Image needs to be replaced by heart
@@ -24,12 +29,14 @@ public class ResourceHeart extends Resource implements Serializable{
 	this.setResourceType("heart");
     }
 
+    /**
+     * @param g Graphics
+     * Draws the image of the resource at its x and y coordinates
+     */
     public void paintResource(Graphics g){
 	//paint resource
 	Graphics2D g2d = (Graphics2D) g;
 	g2d.drawImage(this.getResourceImage().getImage(),(int) this.getXCoord(), (int) this.getYCoord(), null);
 	// implement gravity if not on ground
     }
-
-
 }
