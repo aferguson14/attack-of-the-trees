49a88ea1ca32ad94b4faa7e611e1da7bbb7c753c
Michele Haque <michelehaque@engineering.ucsb.edu>
2015-03-13 10:55:18 -0700

    Updated javadoc comments for methods

--- src/Resource.java
+++ src/Resource.java
@@ -17,47 +17,80 @@ import javax.swing.ImageIcon;
 
 public class Resource implements Serializable{
 
-    private double XCoord, YCoord;
-    private ImageIcon resourceImage;
-    private String resourceType;
+    private double XCoord, YCoord; //x and y coordinates for the resource
+    private ImageIcon resourceImage; //the image of the resource
+    private String resourceType; //string naming the type of resource
+    
+    /**
+     * @param x double for x-coordinate
+     * @param y double for y-coordinate
+     * Constructor for the resource that sets its position
+     */
     public Resource(double x, double y){
 	setXCoord(x);
 	setYCoord(y);
     }
 
+    /**
+     * Method to paint the resources should be overridden by subclasses 
+     */
     public void paintResource(Graphics g){}
 
 
     //SETTERS AND GETTERS
-
+    
+    /**
+     * @param XCoord double x coordinate to set
+     */
     public void setXCoord(double XCoord){
 	this.XCoord = XCoord;
     }
 
+    /**
+     * @return the XCoord 
+     */
     public double getXCoord(){
 	return XCoord;
     }
 
+    /**
+     * @param YCoord double y coordinate to set
+     */	
     public void setYCoord(double YCoord){
 	this.YCoord = YCoord;
     }
 
+    /**
+     * @return the YCoord 
+     */
     public double getYCoord(){
 	return YCoord;
     }
 
+    /**
+     * @param resourceImage the image of the resource to set
+     */
     public void setResourceImage(ImageIcon resourceImage){
 	this.resourceImage = resourceImage;
     }
 
+    /**
+     * @return the resourceImage 
+     */
     public ImageIcon getResourceImage(){
 	return resourceImage;
     }
 
+    /**
+     * @param resourceType the type of resource to set
+     */
     public void setResourceType(String resourceType){
 	this.resourceType = resourceType;
     }
 
+    /**
+     * @return the resourceType 
+     */
     public String getResourceType(){
 	return resourceType;
     }
